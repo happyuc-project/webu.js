9f8a55014715c39603e97b5dac7654fda64199b7
dreamxyp <dreamxyp@gmail.com>
2018-04-18 19:59:44 +0800

    huc
    huc

--- test/utils.fromWei.js
+++ test/utils.fromWei.js
@@ -4,19 +4,25 @@ var utils = require('../lib/utils/utils.js');
 describe('lib/utils/utils', function () {
     describe('fromWei', function () {
         it('should return the correct value', function () {
-
-            assert.equal(utils.fromWei(1000000000000000000, 'wei'),    '1000000000000000000');
+            assert.equal(utils.fromWei(1000000000000000000,  'wei'),   '1000000000000000000');
             assert.equal(utils.fromWei(1000000000000000000, 'kwei'),   '1000000000000000');
             assert.equal(utils.fromWei(1000000000000000000, 'mwei'),   '1000000000000');
             assert.equal(utils.fromWei(1000000000000000000, 'gwei'),   '1000000000');
-            assert.equal(utils.fromWei(1000000000000000000, 'szabo'),  '1000000');
-            assert.equal(utils.fromWei(1000000000000000000, 'finney'), '1000');
-            assert.equal(utils.fromWei(1000000000000000000, 'hucer'),  '1');
-            assert.equal(utils.fromWei(1000000000000000000, 'khucer'), '0.001');
-            assert.equal(utils.fromWei(1000000000000000000, 'grand'),  '0.001');
-            assert.equal(utils.fromWei(1000000000000000000, 'mhucer'), '0.000001');
-            assert.equal(utils.fromWei(1000000000000000000, 'ghucer'), '0.000000001');
-            assert.equal(utils.fromWei(1000000000000000000, 'thucer'), '0.000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'twei'),   '1000000');
+            assert.equal(utils.fromWei(1000000000000000000, 'pwei'),   '1000');
+            assert.equal(utils.fromWei(1000000000000000000,  'huc'),   '1');
+            assert.equal(utils.fromWei(1000000000000000000, 'khuc'),   '0.001');
+            assert.equal(utils.fromWei(1000000000000000000, 'mhuc'),   '0.000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'ghuc'),   '0.000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'thuc'),   '0.000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'phuc'),   '0.000000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'ehuc'),   '0.000000000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'zhuc'),   '0.000000000000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'yhuc'),   '0.000000000000000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'nhuc'),   '0.000000000000000000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'dhuc'),   '0.000000000000000000000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'vhuc'),   '0.000000000000000000000000000000001');
+            assert.equal(utils.fromWei(1000000000000000000, 'uhuc'),   '0.000000000000000000000000000000000001');
         });
     });
 });
--- test/utils.toWei.js
+++ test/utils.toWei.js
@@ -6,32 +6,33 @@ describe('lib/utils/utils', function () {
     describe('toWei', function () {
         it('should return the correct value', function () {
 
-            assert.equal(utils.toWei(1, 'wei'),    '1');
+            assert.equal(utils.toWei(1,  'wei'),   '1');
             assert.equal(utils.toWei(1, 'kwei'),   '1000');
-            assert.equal(utils.toWei(1, 'Kwei'),   '1000');
-            assert.equal(utils.toWei(1, 'babbage'),   '1000');
             assert.equal(utils.toWei(1, 'mwei'),   '1000000');
-            assert.equal(utils.toWei(1, 'Mwei'),   '1000000');
-            assert.equal(utils.toWei(1, 'lovelace'),   '1000000');
             assert.equal(utils.toWei(1, 'gwei'),   '1000000000');
-            assert.equal(utils.toWei(1, 'Gwei'),   '1000000000');
-            assert.equal(utils.toWei(1, 'shannon'),   '1000000000');
-            assert.equal(utils.toWei(1, 'szabo'),  '1000000000000');
-            assert.equal(utils.toWei(1, 'finney'), '1000000000000000');
-            assert.equal(utils.toWei(1, 'hucer'),  '1000000000000000000');
-            assert.equal(utils.toWei(1, 'khucer'), '1000000000000000000000');
-            assert.equal(utils.toWei(1, 'grand'),  '1000000000000000000000');
-            assert.equal(utils.toWei(1, 'mhucer'), '1000000000000000000000000');
-            assert.equal(utils.toWei(1, 'ghucer'), '1000000000000000000000000000');
-            assert.equal(utils.toWei(1, 'thucer'), '1000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'twei'),   '1000000000000');
+            assert.equal(utils.toWei(1, 'pwei'),   '1000000000000000');
+            assert.equal(utils.toWei(1,  'huc'),   '1000000000000000000');
+            assert.equal(utils.toWei(1, 'khuc'),   '1000000000000000000000');
+            assert.equal(utils.toWei(1, 'mhuc'),   '1000000000000000000000000');
+            assert.equal(utils.toWei(1, 'ghuc'),   '1000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'thuc'),   '1000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'phuc'),   '1000000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'ehuc'),   '1000000000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'zhuc'),   '1000000000000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'yhuc'),   '1000000000000000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'nhuc'),   '1000000000000000000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'dhuc'),   '1000000000000000000000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'vhuc'),   '1000000000000000000000000000000000000000000000000000');
+            assert.equal(utils.toWei(1, 'uhuc'),   '1000000000000000000000000000000000000000000000000000000');
 
-            assert.equal(utils.toWei(1, 'kwei'),    utils.toWei(1, 'femtohucer'));
-            assert.equal(utils.toWei(1, 'szabo'),   utils.toWei(1, 'microhucer'));
-            assert.equal(utils.toWei(1, 'finney'),  utils.toWei(1, 'millihucer'));
-            assert.equal(utils.toWei(1, 'milli'),    utils.toWei(1, 'millihucer'));
-            assert.equal(utils.toWei(1, 'milli'),    utils.toWei(1000, 'micro'));
+            // assert.equal(utils.toWei(1, 'kwei'),    utils.toWei(1, 'femtohucer'));
+            // assert.equal(utils.toWei(1, 'szabo'),   utils.toWei(1, 'microhucer'));
+            // assert.equal(utils.toWei(1, 'finney'),  utils.toWei(1, 'millihucer'));
+            // assert.equal(utils.toWei(1, 'milli'),    utils.toWei(1, 'millihucer'));
+            // assert.equal(utils.toWei(1, 'milli'),    utils.toWei(1000, 'micro'));
 
-            assert.throws(function () {utils.toWei(1, 'wei1');}, Error);
+            assert.throws(function () {utils.toWei(1, 'wei');}, Error);
         });
     });
 });
