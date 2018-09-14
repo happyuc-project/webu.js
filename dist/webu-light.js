fe73e52b505f63ae039f37ce50173ba9de05a0b0
ldcc <ldcc@live.com>
2018-09-14 17:54:13 +0800

    build artifact

--- dist/webu-light.js
+++ dist/webu-light.js
@@ -2440,7 +2440,7 @@ module.exports = {
 
 },{"./sha3.js":19,"bignumber.js":"bignumber.js","utf8":85}],21:[function(require,module,exports){
 module.exports={
-    "version": "2.0.1"
+    "version": "2.0.3"
 }
 
 },{}],22:[function(require,module,exports){
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 Package.describe({
   name: 'irchain-io:webu',
-  version: '2.0.1',
+  version: '2.0.3',
   summary: 'IrChain JavaScript API, middleware to talk to a irchain node over RPC',
   git: 'https://github.com/irchain/webu.js',
   // By default, Meteor will default to using README.md for documentation.
